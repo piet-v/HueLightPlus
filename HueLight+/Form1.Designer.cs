16660ee2407189659b0aa8b1100a4c7ef58c4fa3
Piet Vandeput <piet-v@live.com>
2017-10-12 21:34:59 +0200

    fixed bug with default window size

--- HueLight+/Form1.Designer.cs
+++ HueLight+/Form1.Designer.cs
@@ -378,7 +378,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(541, 578);
+            this.ClientSize = new System.Drawing.Size(313, 578);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.idleValueLabel);
